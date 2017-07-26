c0c23123fd3802118d89d7d0f3a8ba6e0e22a097
yidezhangfei <yidezhangfei@163.com>
2017-07-26 14:59:25 +0800

    update

--- test.cpp
+++ test.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <sstream>
+#include <stdio.h>
+using namespace std;
 
-int main() {
-    int a = 1;
-    std::cout << a << std::endl;
+int hex2int(const string& hexStr)
+{
+    char *offset;
+    if(hexStr.length() > 2)
+    {
+        if(hexStr[0] == '0' && hexStr[1] == 'x')
+        {
+            return strtol(hexStr.c_str(), &offset, 0);
+        }
+    }
+    return strtol(hexStr.c_str(), &offset, 16);
+}
+ 
+int main()
+{
+    char dict[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+    string s;
+    s = "13102";
+    char x[3];
+    x[0] = dict[10 >> 2 & 0xf];
+    x[1] = dict[(15) - (10 ^ 9)];
+    x[2] = dict[(139 | 193) % 5];
+    
+    int ret = hex2int(x);
+    std::stringstream ss;
+    std::string str;
+    ss<<ret;
+    ss>>str;
+    
+    s = s.insert(2, str);
+    for (int i = s.length(); i > 0; i--) {
+        cout<<s[i - 1];
+    }
 }
