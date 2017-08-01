07bb11d58aa3f94d4c8fffefac36f990883a60e3
yidezhangfei <yidezhangfei@163.com>
2017-08-01 15:03:28 +0800

    shared_ptr correct use

--- smart_ptr/shared_ptr_custom_del.cpp
+++ smart_ptr/shared_ptr_custom_del.cpp
@@ -16,5 +16,15 @@ int main(int argc, char* argv[]) {
     std::shared_ptr<int> int_ptr_array = make_shared_ptr_array<int>(8);
     std::shared_ptr<int> int_ptr = make_shared_ptr<int>();
     std::shared_ptr<Sample> sample_array = make_shared_ptr_array<Sample>(10);
+
+    {
+        int* rawPtr = new int();
+        std::shared_ptr<int> p1(rawPtr);
+        {
+            //std::shared_ptr<int> p2(rawPtr); // will crash
+            std::shared_ptr<int> p2 = p1;  // correct
+            std::cout << p2.use_count() << std::endl;
+        }
+    } 
     return 0;
 }
