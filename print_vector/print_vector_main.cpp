408ebe5dd89847520cd22bb1dd7de02c28b313bb
lijun <lijun>
2018-08-29 20:10:06 +0800

    update

--- print_vector/print_vector_main.cpp
+++ print_vector/print_vector_main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char* argsv[]) {
    std::vector<int> int_vector = { 1, 2, 3, 4 };
    std::vector<std::string> str_vector = { "I", "LOVE", "YOU" };
 
+   // 注意 只有c++14以上才支持lambda的参数自动推导, gcc 4.8并不支持c++14
    auto print_vector = [] (auto& iter) {
        std::cout << iter << std::endl;
    };
