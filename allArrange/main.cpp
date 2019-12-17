fada2dd2b7b38ec809d131192342b135da482980
lijun <yidezhangfei@163.com>
2019-12-17 14:41:53 +0800

    update

--- allArrange/main.cpp
+++ allArrange/main.cpp
@@ -1,14 +1,44 @@
 #include <iostream>
 #include <vector>
 
-std::vector<std::vector<int>> arrange(int n) {
-    std::vector<int> origin;
-    for (int i = 1; i <= n; i++) {
-        origin.push_back(i);
+using namespace std;
+
+void swap(int& a, int& b) {
+    int t = b;
+    b = a;
+    a = t;
+}
+
+void arrangeInternal(vector<int>& nums, int local, vector<vector<int>>& result) {
+    if (local == nums.size() - 1) {
+        result.emplace_back(nums);
+    } else if (local == nums.size()) {
+        return;
+    } else {
+        for (int i = local; i < nums.size(); i++) {
+            swap(nums[i], nums[local]);
+
+            arrangeInternal(nums, local+1, result);
+
+            swap(nums[i], nums[local]);
+        }
     }
+}
+
+std::vector<std::vector<int>> arrange(vector<int>& nums) {
     std::vector<std::vector<int>> result;
+    arrangeInternal(nums, 0, result);
+    return result;
 }
 
 int main() {
+    vector<int> nums = {1,2,3,4};
+    auto result = arrange(nums);
 
+    for (auto i: result) {
+        for (auto j: i) {
+            std::cout << j << " ";
+        }
+        std::cout << std::endl;
+    }
 }
